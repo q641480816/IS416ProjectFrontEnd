8558573d542823143a0fbaf23a065c12892f9e49
PETERLU <jianan.lu.2015@smu.edu.sg>
2018-03-02 17:00:31 +0800

    Completed the location function(Map) simple version.

--- app/src/main/java/com/is416/smujio/component/fragment/MapFragment.java
+++ app/src/main/java/com/is416/smujio/component/fragment/MapFragment.java
@@ -1,46 +1,52 @@
 package com.is416.smujio.component.fragment;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.is416.smujio.JioActivity;
 import com.is416.smujio.R;
 import com.is416.smujio.util.ActivityManager;
+import com.is416.smujio.util.General;
 
+import java.io.IOException;
 import java.util.List;
-import java.util.Locale;
-
-import static com.google.android.gms.location.LocationServices.*;
 
 /**
  * Created by Gods on 2/26/2018.
  */
 
-public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     GoogleMap mGoogleMap;
     SupportMapFragment mapFrag;
@@ -48,7 +54,9 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleA
     GoogleApiClient mGoogleApiClient;
     Location mLastLocation;
     Marker mCurrLocationMarker;
-
+    LocationManager locationManager;
+    LocationListener locationListenerGPS;
+    GoogleApiClient googleApiClient;
     private Context mContext;
     private LayoutInflater inflater;
     private View mainView;
@@ -57,12 +65,107 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleA
         mainView = inflater.inflate(R.layout.map_fragment, container, false);
         this.inflater = inflater;
 
-        SupportMapFragment fragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        fragment.getMapAsync(this);
-
         init();
         bindView();
         addListener();
+
+        mapFrag = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFrag.getMapAsync(this);
+
+        String context = Context.LOCATION_SERVICE;
+        locationManager = (LocationManager) mContext.getSystemService(context);
+        if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return mainView;
+        }
+
+        if(locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new android.location.LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    //get the latitude
+                    double latitude = location.getLatitude();
+                    //get the longitude
+                    double longitude = location.getLongitude();
+                    // instantiate the class, LatLng
+                    LatLng latLng = new LatLng(latitude, longitude);
+                    //Instantiate the class, Geocoder
+                    Geocoder geocoder = new Geocoder(mContext.getApplicationContext());
+                    try {
+                        List<Address> addressList = geocoder.getFromLocation(latitude,longitude,1);
+                        String str = addressList.get(0).getLocality()+",";
+                        str += addressList.get(0).getCountryName();
+                        mGoogleMap.addMarker(new MarkerOptions().position(latLng).title(str));
+                        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            });
+        }else if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new android.location.LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    //get the latitude
+                    double latitude = location.getLatitude();
+                    //get the longitude
+                    double longitude = location.getLongitude();
+                    // instantiate the class, LatLng
+                    LatLng latLng = new LatLng(latitude, longitude);
+                    //Instantiate the class, Geocoder
+                    Geocoder geocoder = new Geocoder(mContext.getApplicationContext());
+                    try {
+                        List<Address> addressList = geocoder.getFromLocation(latitude,longitude,1);
+                        String str = addressList.get(0).getLocality()+",";
+                        str += addressList.get(0).getCountryName();
+                        mGoogleMap.addMarker(new MarkerOptions().position(latLng).title(str));
+                        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            });
+        }
+
+
+
+
         return mainView;
     }
 
@@ -78,19 +181,21 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleA
 
     }
 
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
-
         // Add a marker in Singapore and move the camera.
-        LatLng Singapore = new LatLng(1.290270, 103.851959);
-        mGoogleMap.addMarker(new MarkerOptions().position(Singapore).title("Marker in Singapore"));
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(Singapore));
+//        LatLng Singapore = new LatLng(1.282375, 103.864273);
+//        mGoogleMap.addMarker(new MarkerOptions().position(Singapore).title("I am here!"));
+//        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Singapore, 15));
+
     }
 
     @Override
     public void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
 
