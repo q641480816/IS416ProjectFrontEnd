1029f5e258da119b163b5e9cf66fe2c2f83cf035
PETERLU <jianan.lu.2015@smu.edu.sg>
2018-03-30 20:05:29 +0800

    update Feature!

--- app/src/main/java/com/is416/smujio/component/fragment/MapFragment.java
+++ app/src/main/java/com/is416/smujio/component/fragment/MapFragment.java
@@ -310,7 +310,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleM
                                 if (e.getId() != General.user.getAccountId()) {
                                     MarkerOptions markerOption = new MarkerOptions().position(new LatLng(e.getLatitude(), e.getLongitude()));
                                     //Custom icon
-                                    markerOption.icon(BitmapDescriptorFactory.fromResource(General.getMarker(e.getType())));
+                                    markerOption.icon(BitmapDescriptorFactory.fromResource(General.getIcon(e.getType())));
                                     Marker currentMarker = mGoogleMap.addMarker(markerOption);
                                     marker_info.put(currentMarker, e);
                                     eventListAdapter.updateOne(e);
